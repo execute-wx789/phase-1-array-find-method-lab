4e3263d538bf9fe17e5c4439b2c009abdde99f20
Yurick <YJakse789@gmail.com>
2023-08-02 10:48:08 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    for (const recordHold of record){
+        if(recordHold.result === "W"){
+            return recordHold.year
+        }
+    }
+}
